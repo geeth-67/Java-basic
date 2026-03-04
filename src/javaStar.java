e860e6eee1a3e7aa070c8298d1237ef727f2cf11
vishwageethanjana <geeth.me67@gmail.com>
2026-03-04 09:10:07 +0530

    Java basic

--- src/javaStar.java
+++ src/javaStar.java
@@ -1,6 +1,13 @@
 public class javaStar {
 
-    for(int i = 0; i <= 5; i ++){
-        for(i)
+    public static void main(String[] args) {
+
+        for(int i = 0; i <= 5; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
