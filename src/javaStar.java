2137060d441ac04cde21e6505b7341ab25e4ceb2
vishwageethanjana <geeth.me67@gmail.com>
2026-03-04 10:02:14 +0530

    Java basic

--- src/javaStar.java
+++ src/javaStar.java
@@ -58,5 +58,52 @@ public class javaStar {
         }
     }
 
+    public static void invertedPyramid() {
+        int n = 5;
 
+        for(int i = n; i >= 1; i--) {
+
+            for(int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= (2*i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void diamond() {
+        int n = 5;
+
+        // Top pyramid
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= (2*i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Bottom inverted pyramid
+        for(int i = n-1; i >= 1; i--) {
+
+            for(int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= (2*i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
