c877ca91b956bf77bf1197259d4f063fe623edf4
vishwageethanjana <geeth.me67@gmail.com>
2026-03-04 09:32:57 +0530

    Java basic

--- src/javaStar2.java
+++ src/javaStar2.java
@@ -7,7 +7,12 @@ public class javaStar2 {
 
     private static void pattern() {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ur Rows : ");
+        int rows = sc.nextInt();
 
+        System.out.println("Enter ur columns : ");
+        int columns = sc.nextInt();
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= columns; j++) {
