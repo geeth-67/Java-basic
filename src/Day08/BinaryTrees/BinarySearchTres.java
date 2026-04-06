21ff56b6d8e569340402283407cd4e5880983f37
vishwageethanjana <geeth.me67@gmail.com>
2026-04-06 16:03:09 +0530

    binary search tree 04/06

--- src/Day08/BinaryTrees/BinarySearchTres.java
+++ src/Day08/BinaryTrees/BinarySearchTres.java
@@ -9,4 +9,20 @@ public class BinarySearchTres {
 
         this.root = null;
     }
+
+    public void insert(int value) {
+
+        if (root == null) {
+            root = new Node(value);
+
+        }else {
+            if (root.data > value) {
+                root.left = new Node(value);
+
+            }else if(root.data < value) {
+                root.right = new Node(value);
+            }
+        }
+
+    }
 }
---
+++ src/Day08/BinaryTrees/Main.java
@@ -0,0 +1,13 @@
+package Day08.BinaryTrees;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         BinarySearchTres bst = new BinarySearchTres();
+
+         bst.insert(10) ;
+
+    }
+}
