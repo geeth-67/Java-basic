610b45e4a8a74742d108fda5dd138414eb43a9e8
vishwageethanjana <geeth.me67@gmail.com>
2026-04-22 10:53:06 +0530

    binary search tree 04/22

--- src/Day08/BinaryTrees/Main.java
+++ src/Day08/BinaryTrees/Main.java
@@ -7,9 +7,20 @@ public class Main {
 
          BinarySearchTres bst = new BinarySearchTres();
 
-         bst.insert(10) ;
-         bst.insert(20);
+         bst.insert(40);
          bst.insert(30);
+         bst.insert(60);
+         bst.insert(15);
+         bst.insert(35);
+         bst.insert(80);
+         bst.insert(50);
+         bst.insert(45);
+         bst.insert(70);
 
+         bst.inOrder();
+        System.out.println("_____________________");
+         bst.preOrder();
+        System.out.println("_____________________");
+         bst.postOrder();
     }
 }
