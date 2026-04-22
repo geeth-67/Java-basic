b126e65fb71c4bfb6258512584fbeac1c8d40fb3
vishwageethanjana <geeth.me67@gmail.com>
2026-04-22 09:24:13 +0530

    binary search tree 04/22

--- src/Day08/BinaryTrees/BinarySearchTres.java
+++ src/Day08/BinaryTrees/BinarySearchTres.java
@@ -10,18 +10,42 @@ public class BinarySearchTres {
         this.root = null;
     }
 
+    // without recursive mode
+//    public void insert(int value) {
+//
+//        if (root == null) {
+//            this.root = new Node(value);
+//        }
+//
+//        if (root.data > value) {
+//            root.left = new Node(value);
+//        }
+//
+//        if(root.data < value) {
+//            root.right = new Node(value);
+//        }
+//    }
+
+
+    // recursive mode
     public void insert(int value) {
 
-        if (root == null) {
-            this.root = new Node(value);
+        root = insertHelper(root , value);
+    }
+
+    private Node insertHelper(Node node , int value) {
+
+        if (node == null) {
+            return new Node(value);
         }
 
-        if (root.data > value) {
-            root.left = new Node(value);
+        if (value < node.data) {
+            node.left = insertHelper(node.left , value);
+
+        }else if (value > node.data) {
+            node.right = insertHelper(node.right , value);
         }
 
-        if(root.data < value) {
-            root.right = new Node(value);
-        }
+        return node;
     }
 }
